8125c813571aa3237789dce7bbcc41ceb5509e7d
mahfuz <mdmahfuzbd@gmail.com>
2021-02-27 21:05:57 +0600

    module 34-11 done

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,7 +1,10 @@
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
     const {name,img,stock,seller,price} = props.product;
+    //console.log(props);
     return (
         <div className='product'>
             <div className='product-image'><img src={img} alt=""/></div>
@@ -11,6 +14,8 @@ const Product = (props) => {
             <p>By: {seller}</p>
             <p>Price: $ {price}</p>
             <p>Stock: {stock}</p>
+            <button className='addbtn' onClick={()=>props.handleAddProduct(props.product)}><FontAwesomeIcon icon={faShoppingCart}/>Add to cart</button>
+            
             </div>                  
         </div>
     );
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -6,13 +6,17 @@ const Shop = () => {
     // console.log(fakeData);
     const first10 = fakeData.slice(0,10);
     const [products,setProducts] = useState(first10);
+    //jekhane state thake shekhai eventhandler function likha valo
+    const handleAddProduct = (product)=>{
+        console.log(product);
+    };
     
     return (
         <div className='shop-container'>
             <div className="product-container">
             {/* <h3>{products.length}</h3> */}
                 {
-                   products.map((pd)=><Product product={pd}></Product>)                
+                   products.map((pd)=><Product product={pd} handleAddProduct = {handleAddProduct}></Product>)                
                 }                
             </div>
             <div className="cart-container">
